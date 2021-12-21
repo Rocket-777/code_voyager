e4d3596cc73cfcdfc01a38791ff53db0d0feb6db
Test258744 <roketir.57@gmail.com>
2021-12-21 09:27:59 +0300

    tweaks

--- frontend/src/userProfile/scripts/usrInit.js
+++ frontend/src/userProfile/scripts/usrInit.js
@@ -8,16 +8,16 @@ async function usrInit(uri, setUsrData){
         referrerPolicy: 'no-referrer',
 
     }).then(res => res.json()).then(res => res).catch(e => console.log(e)); //EXAMPLE OF RETRIVING TEXT DATA
-
-
-    if(user.status === 0 ){
-        setUsrData({username: user.username, status: 'Пользователь', image: user.image});
-    }else if(user.status === 1){
-        setUsrData({username: user.username, status: 'Модератор', image: user.image});
-    }else if(user.status === 2){
-        setUsrData({username: user.username, status: 'Администратор', image: user.image});
+    if(user){
+        if(user.status === 0 ){
+            setUsrData({username: user.username, status: 'Пользователь', image: user.image});
+        }else if(user.status === 1){
+            setUsrData({username: user.username, status: 'Модератор', image: user.image});
+        }else if(user.status === 2){
+            setUsrData({username: user.username, status: 'Администратор', image: user.image});
+        }
+        console.log(user.status);
     }
-    console.log(user.status);
 
 }
 export {usrInit}
