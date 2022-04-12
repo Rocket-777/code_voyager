ad37fa208ecd8eca281978ad2325ffe53380c471
Test258744 <roketir.57@gmail.com>
2022-04-12 09:53:30 +0300

    -minor comment fix

--- frontend/src/placesTape/placeDetailed/index.jsx
+++ frontend/src/placesTape/placeDetailed/index.jsx
@@ -1,6 +1,14 @@
 import {useParams} from "react-router-dom";
-import {Container, StyledCard, StyledHeader, ShortDescription,
-    FullDescription, Info, ActionButtonContainer, MapContainer} from "./styles";
+import {
+    ActionButtonContainer,
+    Container,
+    FullDescription,
+    Info,
+    MapContainer,
+    ShortDescription,
+    StyledCard,
+    StyledHeader
+} from "./styles";
 import {ImageContainer, NoImage} from "../placeCard/styles";
 import {MarginContainer} from "../comments/sendComment/styles";
 import {Footer} from "../../main/footer";
@@ -12,46 +20,50 @@ import {SendComment} from "../comments/sendComment";
 import {getComments} from "../comments/scripts";
 import {PrettyDivider} from "./divider";
 import {ActionButtons} from "../../actionButtons";
-import {likeAction, favoriteAction} from "../placeCard/scripst/placeCardScripts";
+import {favoriteAction, likeAction} from "../placeCard/scripst/placeCardScripts";
 import {Loader} from "../../main/loading";
 
-import {YMaps, Map, Placemark} from "react-yandex-maps";
+import {Map, Placemark, YMaps} from "react-yandex-maps";
 
 const PlaceDetailed = (props) => {
 
     const [placeData, setPlaceData] = useState('');
     const [commentsData, setCommentsData] = useState('');
     const [isLoading, setIsLoading] = useState(true);
-    const [markerPos, setMarker] = useState([54.514,36.26]);
+    const [markerPos, setMarker] = useState([54.514, 36.26]);
     const params = useParams();
     let ac = new AbortController();
-    async function handleLike(){
+
+    async function handleLike() {
         await likeAction(placeData._id);
         await getPlaceById(params.id, setPlaceData, ac);
     }
 
-    async function handleFavorite(){
+    async function handleFavorite() {
         await favoriteAction(placeData._id);
         await getPlaceById(params.id, setPlaceData, ac);
     }
 
-    useEffect(()=>{
+    useEffect(() => {
 
-        getPlaceById(params.id, setPlaceData, ac).then(res => {if(!ac.signal.aborted)setIsLoading(false)});
+        getPlaceById(params.id, setPlaceData, ac).then(res => {
+            if (!ac.signal.aborted) setIsLoading(false)
+        });
         getComments(setCommentsData, params.id, ac);
-        return() => ac.abort();
+        return () => ac.abort();
     }, [])
 
 
-    if(!placeData.error && !isLoading){
-        return(
+    if (!placeData.error && !isLoading) {
+        return (
             <Container id='detailedPlace'>
                 <NavigateTop elemId='detailedPlace'/>
-                <NavigateBack />
+                <NavigateBack/>
                 <StyledCard>
 
                     <ImageContainer>
-                        {placeData.image ? <img src={placeData.image} alt='image'/> : <NoImage variant='h1'>No_Image</NoImage>}
+                        {placeData.image ? <img src={placeData.image} alt='image'/> :
+                            <NoImage variant='h1'>No_Image</NoImage>}
                     </ImageContainer>
                     <StyledHeader variant='h4'>{placeData.place_name}</StyledHeader>
                     <PrettyDivider textVal='Рецензия'/>
@@ -67,41 +79,43 @@ const PlaceDetailed = (props) => {
                         <YMaps>
                             {/*onDragEnd={e => console.log(e.get('target').geometry.getCoordinates())}*/}
 
-                                <Map defaultState={{ center: markerPos, zoom: 14 }} width='100%' height='60vh'>
-                                    <Placemark id='placemark' geometry={markerPos} options={{draggable: true}}
-                                               onDragEnd={e => {console.log(e.get('target').geometry.getCoordinates());
-                                                   setMarker(e.get('target').geometry.getCoordinates());
-                                               }} />
-                                </Map>
+                            <Map defaultState={{center: markerPos, zoom: 14}} width='100%' height='60vh'>
+                                <Placemark id='placemark' geometry={markerPos} options={{draggable: true}}
+                                           onDragEnd={e => {
+                                               console.log(e.get('target').geometry.getCoordinates());
+                                               setMarker(e.get('target').geometry.getCoordinates());
+                                           }}/>
+                            </Map>
 
 
                             <div>{markerPos[0]}</div>
                             <div>{markerPos[1]}</div>
                         </YMaps>
-                   </MapContainer>
+                    </MapContainer>
                     <Info>{'PLACE_ID: ' + params.id + ' _info'}</Info>
                     <ActionButtonContainer>
                         <ActionButtons favoriteVisible={true} isFavorite={placeData.isFavorite}
                                        isLiked={placeData.isLiked} removeVisible={false}
-                                       likeCount={placeData.likes} likeAction={() => handleLike()} favoriteAction={() => handleFavorite()}/>
+                                       likeCount={placeData.likes} likeAction={() => handleLike()}
+                                       favoriteAction={() => handleFavorite()}/>
                     </ActionButtonContainer>
 
                     <Comments data={commentsData}/>
 
                 </StyledCard>
                 <MarginContainer>
-                    <SendComment id={params.id} updateComments={setCommentsData} updatePlaceData={null} ac={ac}/>
+                    <SendComment id={params.id} commentOf='places' updateComments={setCommentsData}
+                                 updatePlaceData={null} ac={ac}/>
                 </MarginContainer>
                 <Footer/>
             </Container>
         );
-    }
-    else if(placeData.error){
-        return(
+    } else if (placeData.error) {
+        return (
             <h1>{placeData.error}</h1>
         );
-    } else if(isLoading){
-        return(
+    } else if (isLoading) {
+        return (
             <Container>
                 <Loader/>
                 <Footer/>
